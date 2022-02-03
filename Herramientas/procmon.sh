2127e08eba921607385ae987d21aa874bc1cbc49
xy1sul41n <74555507+xy1sul41n@users.noreply.github.com>
2022-02-03 17:45:09 +0100

    Create procmon.sh

---
+++ Herramientas/procmon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+old_process=$(ps -eo command)
+
+while true; do
+        new process=$(ps -eo command)
+        diff <(echo "$old_process") <(echo "$new_process") | grep  "[\>\<]" | grep -v -E "procmon|command"
+        old_process=$new_process
+done
